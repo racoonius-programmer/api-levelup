42f0ae5fe23c1c059eec59ef62d60b5b0c912e51
Franco Reyes <fr.reyesh@duocuc.cl>
2025-11-19 13:49:46 -0300

    utilities

---
+++ src/main/resources/crearUsers.sql
@@ -0,0 +1,5 @@
+INSERT INTO `usuarios`(`id`, `comuna`, `contrasena`, `correo`, `descuento_duoc`, `direccion`, `fecha_nacimiento`, `foto_perfil`, `region`, `rol`, `telefono`, `username`)
+VALUES
+(1, 'Santiago', 'admin123', 'admin@gmail.com', FALSE, 'Av. Principal 100', '1990-01-01', '/img/header/user-logo-generic-white-alt.png', 'Región Metropolitana de Santiago', 'admin', '123456789', 'admin'),
+(2, 'Viña del Mar', 'juanito123', 'juanito@duoc.cl', TRUE, 'Calle Secundaria 45', '2000-05-12', '/img/header/user-logo-generic-white-alt.png', 'Valparaíso', 'usuario', '987654321', 'juanito'),
+(3, 'Concepción', 'maria123', 'maria@gmail.com', FALSE, 'Av. Las Flores 12', '1995-09-20', '/img/header/user-logo-generic-white-alt.png', 'Región del Biobío', 'usuario', '555555555', 'maria');
