b48b2c7845ba9d69d4cb012f4241afb35a31dce3
Franco Reyes <fr.reyesh@duocuc.cl>
2025-11-20 17:43:30 -0300

    sql pedido

---
+++ src/main/resources/crearPedido.sql
@@ -0,0 +1,9 @@
+INSERT INTO pedidos (fecha, cliente_id, productos, estado, total) VALUES (
+  '2025-11-20T12:34:56Z',
+  2,
+  '[{"codigo":"FG001","nombre":"Frodo","cantidad":2,"precio":29990.0},
+    {"codigo":"JM002","nombre":"Carcassonne","cantidad":1,"precio":24990.0},
+    {"codigo":"PP001","nombre":"Polera Gamer Personalizada Level-Up","cantidad":1,"precio":14990.0}]',
+  'en preparacion',
+  99960.00
+);
